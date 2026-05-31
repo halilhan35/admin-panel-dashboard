92207995997466da309b8c0df541e785720825f2
halilhan35 <152743712+halilhan35@users.noreply.github.com>
2026-05-31 16:20:46 +0300

    fix: node_modules ve gereksiz dosyalar gitignore'a alındı

--- api/db/models/Roles.js
+++ api/db/models/Roles.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+const RolePrivileges = require("./RolePrivileges");
 
 const schema = mongoose.Schema({
-  role_name: {type: String, required: true},
+  role_name: {type: String, required: true, unique: true},
   is_active: {type: Boolean, default: true},
   created_by: {
     type: mongoose.SchemaTypes.ObjectId,
-    required: true
   }
 },{
    versionKey: false,
@@ -15,7 +15,12 @@ const schema = mongoose.Schema({
   } 
 });
 
-class Roles extends mongoose.Model{
+class Roles extends mongoose.Model {
+
+    static async deleteWithPrivileges(id) {
+        await RolePrivileges.deleteMany({ role_id: id });
+        await this.deleteOne({ _id: id });
+    }
 
 }
 
